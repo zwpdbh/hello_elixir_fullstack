c75ad699dbd89d21c7e93c7eb069bfcd372e9e4e
zwpdbh <zw>
2024-05-29 18:25:30 +0800

    > ready

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,7 @@
 [
   import_deps: [:ecto, :ecto_sql, :phoenix, :absinthe],
   subdirectories: ["priv/*/migrations"],
-  plugins: [Phoenix.LiveView.HTMLFormatter, "Absinthe.Formatter"],
+  plugins: [Phoenix.LiveView.HTMLFormatter, Absinthe.Formatter],
   inputs: [
     "*.{heex,ex,exs}",
     "{config,lib,test}/**/*.{heex,ex,exs}",
