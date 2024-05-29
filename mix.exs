003cd12c4ef1746c25478e804a7b42279105a1e2
zwpdbh <zw>
2024-05-29 18:23:00 +0800

    <installed deps and configured fmt

--- mix.exs
+++ mix.exs
@@ -57,7 +57,9 @@ defmodule Hello.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
